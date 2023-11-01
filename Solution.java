6f6e4b321872f6fd92eb2ff962431234257f6049
Herman <julian.herman@fiserv.com>
2023-11-01 15:43:34 -0400

    implemented linkedListValues()

--- Solution.java
+++ Solution.java
@@ -1,6 +1,5 @@
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.transform.Source;
 
 class Node<T> {
   T val;
@@ -14,11 +13,22 @@ class Node<T> {
 
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+    var ptr = head;
+    var result = new ArrayList<String>();
+    if ( head == null ) return result;
+
+    while ( ptr.next != null ) {
+      result.add(ptr.val);
+      ptr = ptr.next;
+    }
+    result.add(ptr.val);
+
+    return result;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
+    System.out.println("First test:");
+
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -26,10 +36,31 @@ class Solution {
     a.next = b;
     b.next = c;
     c.next = d;
-
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
     // -> [ "a", "b", "c", "d" ]
+    for ( var value: Solution.linkedListValues(a)) {
+      System.out.print(value + " ");
+    }
+
+
+
+    System.out.println("\nAnother test: ");
+
+    Node<String> e = new Node<>("e");
+    Node<String> z = new Node<>("z");
+    Node<String> q = new Node<>("q");
+    Node<String> x = new Node<>("x");
+    Node<String> h = new Node<>("h");
+    e.next = z;
+    z.next = q;
+    q.next = x;
+    x.next = h;
+    // e -> z -> q -> x -> h
+
+    // -> [ "e", "z", "q", "x", "h" ]
+    for ( var value: Solution.linkedListValues(e)) {
+      System.out.print(value + " ");
+    }
   }
 }
